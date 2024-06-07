b1aecb9d393cb6f90000038391d81a1eee1e4ce9
MayerlyMunoz123 <mml.munoz@yavirac.edu.ec>
2024-06-07 01:01:23 -0500

    imagen2

--- lib/Compras.dart
+++ lib/Compras.dart
@@ -4,27 +4,36 @@ class Producto {
   final String nombre;
   final String descripcion;
   final double precio;
+  final String images; 
 
-
-  Producto({required this.nombre, required this.descripcion, required this.precio});
+  Producto({
+    required this.nombre,
+    required this.descripcion,
+    required this.precio,
+    required this.images,
+  });
 }
 
 class ComprasPage extends StatelessWidget {
   final List<Producto> productos = [
     Producto(
-        nombre: 'Producto 1',
-        descripcion: 'Descripción del producto 1',
-        precio: 10.0,
-        ),
-    Producto(
-        nombre: 'Producto 2',
-        descripcion: 'Descripción del producto 2',
-        precio: 20.0,
+      nombre: 'Producto 1',
+      descripcion: 'Descripción del producto 1',
+      precio: 10.0,
+      images: 'images/4.jpg',
     ),
     Producto(
-        nombre: 'Producto 3',
-        descripcion: 'Descripción del producto 3',
-        precio: 30.0,),
+      nombre: 'Producto 2',
+      descripcion: 'Descripción del producto 2',
+      precio: 20.0,
+      images: 'images/4.jpg',
+    ),
+    Producto(
+      nombre: 'Producto 3',
+      descripcion: 'Descripción del producto 3',
+      precio: 30.0,
+      images: 'images/4.jpg',
+    ),
   ];
 
   @override
@@ -42,6 +51,11 @@ class ComprasPage extends StatelessWidget {
             margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: ListTile(
               contentPadding: const EdgeInsets.all(16),
+              leading: Image.asset(producto.images, // Mostrar la imagen
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
               title: Text(
                 producto.nombre,
                 style: const TextStyle(
@@ -61,4 +75,10 @@ class ComprasPage extends StatelessWidget {
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ComprasPage(),
+  ));
 }
